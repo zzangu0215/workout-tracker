a939a9163506029936122e439428de7c3848464d
Junny <tajo0215@gmail.com>
2021-08-09 21:40:15 -0700

    apiRoutes implemented, bugs from seed.js fixed

--- models/workout.js
+++ models/workout.js
@@ -39,6 +39,6 @@ const workoutSchema = new Schema({
   ],
 });
 
-const Workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("workout", workoutSchema);
 
 module.exports = Workout;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,48 @@
 const router = require("express").Router();
 
+const Workout = require("../models/workout");
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+    const workoutsData = await Workout.find();
+    res.json(workoutsData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+  try {
+    const workoutId = req.params.id;
+
+    const workoutData = await Workout.findByIdAndUpdate(workoutId, {
+      $push: {
+        exercises: req.body,
+      },
+    });
+
+    res.json(workoutData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/api/workouts", async (req, res) => {
+  try {
+    const workoutData = await Workout.create(req.body);
+    res.json(workoutData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    const rangeData = await Workout.find();
+    res.json(rangeData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
