ff4e12f62c4827c71420bdb71a7c21c3ee7e4ce4
Junny <tajo0215@gmail.com>
2021-08-09 20:33:20 -0700

    model - workout.js is implemented

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,42 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const workoutSchema = new Schema({});
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for exercise",
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type",
+      },
+      distance: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+        required: "Enter a duration",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
